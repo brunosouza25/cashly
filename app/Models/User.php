78977f483241d7fe54124303556d0af1f2d4d9f2
Bruno Souza <brunopw25@gmail.com>
2026-03-17 22:14:32 -0300

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Support\Str;
+use Laravel\Sanctum\HasApiTokens;
+
+#[Hidden(['password', 'remember_token'])]
+class User extends Authenticatable
+{
+    protected $fillable = ['name', 'email', 'password'];
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    /** @use HasFactory<UserFactory> */
+    use HasFactory, Notifiable, HasUuids, HasApiTokens;
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+
+    public function newUniqueId(): string
+    {
+        return (string) Str::uuid7();
+    }
+}
